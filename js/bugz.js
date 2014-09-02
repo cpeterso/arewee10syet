3510cdeda61e75e2f0ec4553ed4436fc7200f428
Chris Peterson <cpeterson@mozilla.com>
2014-09-02 00:53:29 -0700

    Burninate v1

--- js/bugz.js
+++ js/bugz.js
@@ -6,17 +6,267 @@
         password: "",
     });
 
+    function xhr(url, callback) {
+        function onError(event) {
+            console.error(event.type);
+            request.removeEventListener("error", onError);
+            request.removeEventListener("load", onLoad);
+            callback(event, null);
+        }
+
+        function onLoad(event) {
+            //console.debug("onLoad: " + request.responseText);
+            request.removeEventListener("error", onError);
+            request.removeEventListener("load", onLoad);
+            var response = JSON.parse(request.responseText);
+            callback(null, response);
+        }
+
+        var request = new XMLHttpRequest();
+        request.addEventListener("error", onError);
+        request.addEventListener("load", onLoad);
+        request.open("GET", url, true);
+        request.setRequestHeader("Accept", "application/json");
+        request.send();
+    }
+
+    function storyPointsFromHours(hours) {
+        if (hours <= 0) {
+            return 0;
+        }
+        var days = hours / 8;
+        var f1 = 1;
+        var f2 = 1;
+        while (f1 < days) {
+            var fn = f1 + f2;
+            f1 = f2;
+            f2 = fn;
+        }
+        return f1;
+    }
+
+    function matchRealName(detail) {
+        // "Joe Cool (:joe :joecool)"
+        return detail.real_name.match(/\s*(.+)\s+[([]:(\S+).*[)\]]\s*/);
+    }
+
+    function getAssignedToName(detail) {
+        var match = matchRealName(detail);
+        return (match && match.length >= 2) ? match[1] : "";
+    }
+
+    function getAssignedToNick(detail) {
+        var match = matchRealName(detail);
+        return (match && match.length >= 3) ? match[2] : null;
+    }
+
+    function materializePerson(detail) {
+        return {
+            name: getAssignedToName(detail),
+            nick: getAssignedToNick(detail),
+            email: detail.email,
+        };
+    }
+
+    function materializeTimeTracking(bug) {
+        if (typeof bug.estimated_time === "undefined") {
+            return null; // no time tracking data
+        }
+        var currentEstimate = bug.actual_time + bug.remaining_time;
+        return {
+            originalEstimate: bug.estimated_time,
+            currentEstimate: currentEstimate,
+            remainingHours: bug.remaining_time,
+            storyPoints: storyPointsFromHours(bug.estimated_time), // if no Points flag or p=# whiteboard tag
+        };
+    }
+
+    function materializeBlockingFlags(bug) {
+        // "cf_blocking_b2g" : "---",
+        // "cf_blocking_fennec" : "---",
+        // "cf_blocking_fennec10" : "+",
+        // "cf_blocking_fx" : "---",
+        var cf_blocking_ = "cf_blocking_";
+        var cf_blocking_len = cf_blocking_.length;
+        return _.reduce(bug, function(blockingFlags, value, key) {
+            if (value !== "---" && key.startsWith(cf_blocking_)) {
+                key = key.slice(cf_blocking_len);
+                blockingFlags[key] = value;
+            }
+            return blockingFlags;
+        }, {});
+    }
+
+    function materializeStatusFlags(bug) {
+        // "cf_status_b2g_2_1" : "---",
+        // "cf_status_firefox30" : "unaffected",
+        // "cf_status_firefox31" : "affected",
+        // "cf_status_firefox32" : "fixed",
+        var cf_status_ = "cf_status_";
+        var cf_status_len = cf_status_.length;
+        return _.reduce(bug, function(statusFlags, value, key) {
+            if (value !== "---" && key.startsWith(cf_status_)) {
+                key = key.slice(cf_status_len);
+                statusFlags[key] = value;
+            }
+            return statusFlags;
+        }, {});
+    }
+
+    function materializeTrackingFlags(bug) {
+        // "cf_tracking_b2g_v1_3" : "---",
+        // "cf_tracking_e10s" : "later",
+        // "cf_tracking_firefox30" : "+",
+        var cf_tracking_ = "cf_tracking_";
+        var cf_tracking_len = cf_tracking_.length;
+        return _.reduce(bug, function(trackingFlags, value, key) {
+            if (value !== "---" && key.startsWith(cf_tracking_)) {
+                key = key.slice(cf_tracking_len);
+                trackingFlags[key] = value;
+            }
+            return trackingFlags;
+        }, {});
+    }
+
+    function materializeBug(bug) {
+        return {
+            assignedTo: materializePerson(bug.assigned_to_detail),
+            blockingFlags: materializeBlockingFlags(bug),
+            blocks: bug.blocks,
+            component: bug.component,
+            dependsOn: bug.depends_on,
+            dupeOf: bug.dupe_of,
+            id: bug.id,
+            keywords: bug.keywords,
+            open: bug.is_open,
+            os: bug.op_sys,
+            product: bug.product,
+            reportedAt: new Date(bug.creation_time),
+            reporter: materializePerson(bug.creator_detail),
+            resolution: bug.resolution,
+            summary: bug.summary,
+            status: bug.status,
+            statusFlags: materializeStatusFlags(bug),
+            timeTracking: materializeTimeTracking(bug),
+            trackingFlags: materializeTrackingFlags(bug),
+            whiteboard: bug.whiteboard, // TODO: parse whiteboard tags?
+            _XXX: bug, // escape hatch to original bug object
+        };
+    }
+
+    function searchBugs(searchTerms, callback) {
+        var url = [BUGZILLA_URL, "bug?"];
+        for (var i = 0; i < searchTerms.length; ) {
+            var key = searchTerms[i++];
+            var value = searchTerms[i++];
+            url.push("&", encodeURIComponent(key), "=", encodeURIComponent(value));
+        }
+        if (loginToken) {
+            url.push("&token=", loginToken);
+        }
+        // Must use exclude_fields because we can't include_fields all cf_status_* or cf_tracking_* flags by name.
+        url.push("&exclude_fields=alias,cc,cf_crash_signature,cf_qa_whiteboard,cf_user_story,classification,flags,groups,is_cc_accessible,is_confirmed,is_creator_accessible,platform,priority,qa_contact,see_also,severity,target_milestone,url,version");
+        // last_change_time?
+        url = url.join("");
+        xhr(url, function(error, response) {
+            if (error) {
+                callback(error, null);
+            } else {
+                var bugs = _.map(response.bugs, materializeBug);
+                callback(null, bugs);
+            }
+        });
+    }
+
+    var BUGZILLA_URL = "https://bugzilla.mozilla.org/rest/";
+    var loginToken = null;
+
     exports.$bugz = {
+        login: function(username, password, callback) {
+            username = encodeURIComponent(username);
+            password = encodeURIComponent(password);
+            xhr(BUGZILLA_URL + "login?login=" + username + "&password=" + password,
+                function(error, response) {
+                    error = error || (response.error && response.message);
+                    if (error) {
+                        callback(error, null);
+                    } else {
+                        loginToken = encodeURIComponent(response.token);
+                        callback(null, response);
+                    }
+                });
+        },
+        searchBugs: searchBugs,
+        getBugs: function(bugIDs, callback) {
+            var searchTerms = _.reduce(bugIDs, function(searchTerms, bugID) {
+                searchTerms.push($bugz.field.ID, bugID);
+                return searchTerms;
+            }, []);
+            searchBugs(searchTerms, callback);
+        },
+        getBug: function(bugID, callback) {
+            $bugz.getBugs([bugID], function(error, bugs) {
+                var bug = bugs ? bugs[0] : null;
+                callback(error, bug);
+            });
+        },
+        getBugComments: function(bugID, callback) {
+            var url = BUGZILLA_URL + "bug/" + bugID + "/comment";
+            if (loginToken) {
+                url += "?token=" + loginToken;
+            }
+            url += "&include_fields=creator,time,raw_text";
+            xhr(url, function(error, response) {
+                if (error) {
+                    callback(error, null);
+                } else {
+                    var comments = _.map(response.bugs[bugID].comments, function(comments, value) {
+                        return {
+                            commenter: value.creator,
+                            time: new Date(value.time),
+                            text: value.raw_text,
+                        };
+                    });
+                    console.debug(comments);
+                    //callback(error, comments);
+                }
+            });
+        },
+        getBugHistory: function(bugID, callback) {
+            var url = BUGZILLA_URL + "bug/" + bugID + "/history";
+            if (loginToken) {
+                url += "?token=" + loginToken;
+            }
+            //url += "&include_fields=creator,time,raw_text";
+            xhr(url, function(error, response) {
+                if (error) {
+                    callback(error, null);
+                } else {
+                    var comments = _.map(response.bugs[bugID].comments, function(comments, value) {
+                        return {
+                            commenter: value.creator,
+                            time: new Date(value.time),
+                            text: value.raw_text,
+                        };
+                    });
+                    console.debug(comments);
+                    //callback(error, comments);
+                }
+            });
+        },
+        makeURL: function(bugID) {
+            return "https://bugzilla.mozilla.org/show_bug.cgi?id=" + bugID;
+        },
         status: {
-            UNCONFIRMED: "UNCONFIRMED",
-            NEW: "NEW",
             ASSIGNED: "ASSIGNED",
+            NEW: "NEW",
             REOPENED: "REOPENED",
             RESOLVED: "RESOLVED",
+            UNCONFIRMED: "UNCONFIRMED",
             VERIFIED: "VERIFIED",
         },
         resolution: {
-            NONE: "",
+            // null?
             DUPLICATE: "DUPLICATE",
             FIXED: "FIXED",
             INCOMPLETE: "INCOMPLETE",
@@ -24,11 +274,23 @@
             WONTFIX: "WONTFIX",
             WORKSFORME: "WORKSFORME",
         },
-        makeURL: function(bugId) {
-            return "https://bugzilla.mozilla.org/show_bug.cgi?id=" + bugId;
+        field: {
+            ASSIGNEE: "assigned_to",
+            BLOCKS: "blocks",
+            COMPONENT: "component",
+            DEPENDS_ON: "deponds_on",
+            HOURS_LEFT: "remaining_time",
+            HOURS_WORKED: "actual_time",
+            ID: "id",
+            KEYWORDS: "keywords",
+            ORIGINAL_ESTIMATE: "estimated_time",
+            PRODUCT: "product",
+            REPORTED: "creation_time",
+            REPORTER: "creator",
+            RESOLUTION: "resolution",
+            STATUS: "status",
+            SUMMARY: "summary",
+            WHITEBOARD: "whiteboard",
         },
-        searchBugs: function(params, callback) {
-            bzClient.searchBugs(params, callback);
-        }
     };
 })(this);
---
+++ js/burndown.js
@@ -0,0 +1,254 @@
+;(function(exports) {
+    "use strict";
+
+    // https://www.mozilla.org/en-US/styleguide/identity/firefox/color/
+    const FIREFOX_ORANGE = "#E66000";
+    const FIREFOX_LIGHT_ORANGE = "#FF9500";
+    const FIREFOX_YELLOW = "#FFCB00";
+    const FIREFOX_BLUE = "#00539F";
+    const FIREFOX_LIGHT_BLUE = "#0095DD";
+    const FIREFOX_LIGHT_BLUE_GREY1 = "#EAEFF2";
+    const FIREFOX_LIGHT_BLUE_GREY2 = "#D4DDE4";
+    const FIREFOX_DARK_BLUE_GREY1 = "#424F5A";
+    const FIREFOX_DARK_BLUE_GREY2 = "#6A7B86";
+
+    const E10S_M1 = 997456;
+    const E10S_M2 = 997462;
+    const E10S_ADDONS = 905436;
+    const SHUMWAY_M2= 1044759;
+    const SHUMWAY_M3 = 1037568;
+
+    function log(s) {
+       console.debug(s);
+    }
+
+    function parseQueryString() {
+        // e.g. "?foo=bar&baz=qux&/"
+        var query = window.location.search;
+        if (query.length <= 1) {
+            return {};
+        }
+
+        var slash = (query[query.length - 1] === '/') ? -1 : undefined;
+        query = query.slice(1, slash);
+
+        var kvs = {};
+
+        var params = query.split("&");
+        _.forEach(params, function(kv) {
+            kv = kv.split("=", 2);
+            var key = kv[0];
+            if (key.length === 0) {
+                return; // "&&"
+            }
+            var value = (kv.length > 1) ? decodeURIComponent(kv[1]) : null;
+            kvs[key] = value;
+        });
+        return kvs;
+    }
+
+    function getElementValue(id) {
+        return document.getElementById(id).value;
+    }
+
+    function daysFromHours(hours) {
+        return Math.ceil(hours / 8);
+    }
+
+    function weeksFromHours(hours) {
+        return Math.ceil(daysFromHours(hours) / 5);
+    }
+
+    function calendarDaysFromWorkDays(workDays) {
+        var workWeeks = workDays / 5;
+        var modWorkDays = workDays % 5;
+        return workWeeks * 7 + modWorkDays;
+    }
+
+    function yyyy_mm_dd(date) {
+        return date.toISOString().slice(0,10);
+    }
+
+    function burndownOpenClosed(data) {
+        c3.generate({
+            data: {
+                x: "x",
+                columns: [
+                    ["x"].concat(data.dates),
+                    ["open"].concat(data.open),
+                    ["closed"].concat(data.closed),
+                    ["days"].concat(data.days),
+                ],
+                names: {
+                    open: "Open Bugs",
+                    closed: "Closed Bugs",
+                    days: "Days Remaining",
+                },
+                types: {
+                    open: "area",
+                    closed: "area",
+                    days: "line",
+                },
+                colors: {
+                    open: FIREFOX_LIGHT_ORANGE,
+                    closed: FIREFOX_LIGHT_BLUE_GREY2,
+                    days: FIREFOX_BLUE,
+                },
+                groups: [["open", "closed"]],
+                order: "asc",
+            },
+            axis: {
+                x: {
+                    type: "timeseries",
+                    tick: {
+                        format: "%m-%d"
+                    }
+                }
+            },
+        });
+    }
+
+    function searchDependentBugs(bug) {
+        $bugz.searchBugs([$bugz.field.BLOCKS, metabug], function(error, bugs) {
+            if (error) {
+                console.error("searchBugs: " + error);
+                return;
+            }
+
+            if (bugs.length === 0) {
+                console.info("searchBugs: zarro boogs");
+                return;
+            }
+
+            var changes = {};
+
+            function getChange(date) {
+                date = yyyy_mm_dd(date);
+                var change = changes[date];
+                if (!change) {
+                    change = {date: date, bugsOpened: [], bugsClosed: []};
+                    changes[date] = change;
+                }
+                return change;
+            }
+
+            _.forEach(bugs, function(bug) {
+//                log("bug " + bug.id + ": " + bug.reportedAt);
+
+                getChange(bug.reportedAt).bugsOpened.push(bug);
+
+                if (!bug.open) {
+                    // XXX pretend last change time is time of resolution
+                    var lastChangeTime = new Date(bug._XXX.last_change_time);
+                    getChange(lastChangeTime).bugsClosed.push(bug);
+                }
+            });
+
+            var bugDates = [];
+            var openBugCounts = [];
+            var closedBugCounts = [];
+            var remainingDays = [];
+
+            var runningOpenBugCount = 0;
+            var runningClosedBugCount = 0;
+            var runningRemainingDays = 0;
+
+            changes = _.sortBy(changes, "date");
+
+            // six months ago
+            const MS_PER_DAY = 24*60*60*1000;
+            const MS_PER_MONTH = 4*7*MS_PER_DAY;
+            var chartStartDate = yyyy_mm_dd(new Date(Date.now() - 6 * MS_PER_MONTH));
+
+            _.forEach(changes, function(change) {
+                var closedBugCountDelta = change.bugsClosed.length;
+                var openBugCountDelta = change.bugsOpened.length - closedBugCountDelta;
+
+                var daysOpened = daysFromHours(_.reduce(change.bugsOpened, function(sum, bug) {
+                    var t = bug.timeTracking;
+                    // FIXME: log warning if no timeTracking?
+                    return sum + (t ? t.currentEstimate : 0);
+                }, 0));
+
+                var daysClosed = daysFromHours(_.reduce(change.bugsClosed, function(sum, bug) {
+                    var t = bug.timeTracking;
+                    // FIXME: log warning if no timeTracking?
+                    return sum + (t ? t.currentEstimate : 0);
+                }, 0));
+
+                runningOpenBugCount += openBugCountDelta;
+                runningClosedBugCount += closedBugCountDelta;
+                runningRemainingDays += daysOpened - daysClosed;
+
+                if (change.date >= chartStartDate) {
+                    bugDates.push(change.date);
+                    openBugCounts.push(runningOpenBugCount);
+                    closedBugCounts.push(runningClosedBugCount);
+                    remainingDays.push(runningRemainingDays);
+                }
+            });
+
+            if (people > 0) {
+                var zarroDate = Date.parse(_.last(bugDates));
+                zarroDate += calendarDaysFromWorkDays(runningRemainingDays) * MS_PER_DAY / people;
+                zarroDate = yyyy_mm_dd(new Date(zarroDate));
+                bugDates.push(zarroDate);
+                openBugCounts.push(0);
+                closedBugCounts.push(runningClosedBugCount + runningOpenBugCount);
+                remainingDays.push(0);
+            }
+
+            burndownOpenClosed({
+                dates: bugDates,
+                open: openBugCounts,
+                closed: closedBugCounts,
+                days: remainingDays,
+            });
+        });
+    }
+
+    function login(username, password) {
+        $bugz.login(username, password, function(error, response) {
+            if (error) {
+                console.error("login: " + error);
+                alert(error);
+                return;
+            }
+            searchDependentBugs(metabug);
+        });
+    }
+
+    var query = parseQueryString();
+    var people = +query.people || 0;
+
+    var username = document.getElementById("username");
+    if (query.username) {
+        username.value = query.username;
+    }
+    username.select();
+
+    var password = document.getElementById("password");
+    if (query.password) {
+        password.value = query.password;
+    }
+
+    var button = document.getElementById("button");
+    button.addEventListener("click", function() {
+        var username = getElementValue("username");
+        var password = getElementValue("password");
+        metabug = getElementValue("bug");
+        if (username && password) {
+            login(username, password);
+        } else {
+            searchDependentBugs(metabug);
+        }
+    });
+
+    var metabug = query.bug|0 || query.id|0;
+    if (metabug) {
+        searchDependentBugs(metabug);
+    } else {
+        metabug = E10S_M2;
+    }
+    button.value = metabug;
+})(this);

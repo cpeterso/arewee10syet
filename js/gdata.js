f4ae3c517e2e1c43f8472d5bcd30d15f0e51f869
Chris Peterson <cpeterso@cpeterso.com>
2015-02-23 19:14:09 -0800

    Chrome 40 does not permit `const` in strict mode

--- js/gdata.js
+++ js/gdata.js
@@ -3,12 +3,12 @@
 
     exports.$gdata = {
         parseSpreadsheet: function(json) {
-            const entries = json.feed.entry || [];
+            var entries = json.feed.entry || [];
             return _.map(entries, function(entry) {
                 // e.g. "bug: 1002880, reason: AMO #8, url: https://addons.mozilla.org/en-US/firefox/addon/wot-safe-browsing-tool"
-                const title = (entry.title.type === "html") ? entry.title.$t : encodeURIComponent(entry.title.$t);
-                const row = {title:title};
-                const content = entry.content.$t.split(", ");
+                var title = (entry.title.type === "html") ? entry.title.$t : encodeURIComponent(entry.title.$t);
+                var row = {title:title};
+                var content = entry.content.$t.split(", ");
 
                 _.forEach(content, function(column) {
                     var kv = column.split(": ");
